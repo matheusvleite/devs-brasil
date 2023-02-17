a66ed2deff006c7922c615b266851dd8b5697921
Matheus Victor <matheusvictor2000@gmail.com>
2023-02-17 18:57:18 -0300

    Middleware de atualização de usuario

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { getAllUsers, getCurrentUser, login, register } from "../controllers/UserController.js";
+import { getAllUsers, getCurrentUser, login, register, update } from "../controllers/UserController.js";
 import { authGuard } from "../middlewares/authGuard.js";
 import { validate } from "../middlewares/handleValidation.js";
-import { loginValidation, userCreateValidation } from "../middlewares/userValidation.js";
+import { imageUpload } from "../middlewares/imageUpload.js";
+import { loginValidation, userCreateValidation, userUpdateValidate } from "../middlewares/userValidation.js";
 
 export const userRoutes = express();
 
 userRoutes.post('/register', userCreateValidation(), validate, register);
 userRoutes.post('/login', loginValidation(), validate, login);
-userRoutes.get("/profile",authGuard, getCurrentUser);
+userRoutes.get("/profile", authGuard, getCurrentUser);
+userRoutes.put("/", authGuard, userUpdateValidate(), validate, imageUpload.single("profileImage"), update)
 userRoutes.get("/", getAllUsers)
