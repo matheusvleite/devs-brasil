1d64ae6a5f4c847ae306600e2f4d2aa1ab53e324
Matheus Victor <matheusvictor2000@gmail.com>
2023-02-17 15:19:55 -0300

    Iniciando validações e criando a validação de usuario

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { register } from "../controllers/UserController.js";
+import { validate } from "../middlewares/handleValidation.js";
+import { userCreateValidation } from "../middlewares/userValidation.js";
 
 export const userRoutes = express();
 
-userRoutes.post('/register', register)
+userRoutes.post('/register', userCreateValidation(), validate, register);
