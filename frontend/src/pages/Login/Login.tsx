403531a56b558e348dca384369eb85ec0c411cef
unknown <matheusvictor2000@gmail.com>
2023-02-23 17:45:34 -0300

    Html da pagina de login

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { FormEvent, useState } from 'react';
+import styles from './Login.module.css';
 
 const Login = () => {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password
+    }
+
+
+  }
+
   return (
-    <div>
-        <h2>Login</h2>
+    <div className={styles.login}>
+      <h2 className='subtitle'>Entre na plataforma <span className='logo'>Devs Brasil</span>.</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>E-mail:</span>
+            <input type="email" placeholder='Seu e-mail' value={email} onChange={e => setEmail(e.target.value)}/>
+          </label>
+          <label>
+            <span>Senha:</span>
+            <input type="password" placeholder='Senha' value={password} onChange={e => setPassword(e.target.value)}/>
+          </label>
+          <input type="submit" value="Entrar" />
+        </form>
     </div>
   )
 }
