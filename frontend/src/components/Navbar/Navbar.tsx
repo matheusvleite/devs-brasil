22867d1ea174e0a3ffe8b1a9ac3c901bb2027841
unknown <matheusvictor2000@gmail.com>
2023-02-23 15:42:12 -0300

    Adicionando classes do tema dark e fazendo a função de mudar o tema

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,19 +1,39 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import { BsLightbulbFill, BsLightbulbOffFill } from "react-icons/bs";
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [theme, setTheme] = useState(false);
+
+    const handleTheme = () => {
+        const body = document.querySelector('body');
+        let currentTheme = body?.getAttribute('data-theme')!;
+
+        if(currentTheme === 'light') {
+            currentTheme = 'dark';
+            body?.setAttribute('data-theme', currentTheme)
+            setTheme(true)
+            return
+        }
+
+        body?.setAttribute('data-theme', 'light')
+        setTheme(false)
+    }
+
     return (
         <header>
             <nav className={styles.navbar}>
-                <span>Devs Brasil</span>
+                <Link to='/' className={styles.logo}>Devs Brasil</Link>
                 <ul>
                     <li><NavLink to="/">Início</NavLink></li>
                     <li><NavLink to="/login">Entrar</NavLink></li>
                     <li><NavLink to="/signup">Cadastre-se</NavLink></li>
+                    <li>{theme ? <BsLightbulbOffFill onClick={handleTheme}/> : <BsLightbulbFill onClick={handleTheme}/> }</li>
                 </ul>
             </nav>
         </header>
     )
 }
 
-export default Navbar
+export default Navbar;
