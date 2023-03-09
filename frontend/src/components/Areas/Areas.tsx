c0283032feb71b4ccf2e88c7b69c384608eedb7e
unknown <matheusvictor2000@gmail.com>
2023-03-09 14:40:37 -0300

    CSS de areas e corrigindo tamanhos

--- frontend/src/components/Areas/Areas.tsx
+++ frontend/src/components/Areas/Areas.tsx
@@ -1,12 +1,15 @@
-import { Link } from 'react-router-dom'
-import { areas } from '../../utils/area'
+import { BsPinAngleFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { areas } from '../../utils/area';
+import styles from './Areas.module.css';
 
 const Areas = () => {
     return (
-        <div>
+        <div className={styles.areas}>
+            <h2>Encontre o Profissionais por área</h2>
             <ul>
                 {areas.map(area => (
-                    <li key={area.id}><Link to={`/search?q=${area.name}`}>{area.name}</Link></li>
+                    <li key={area.id}><Link to={`/search?q=${area.name}`}><BsPinAngleFill /> {area.name}</Link></li>
                 ))}
             </ul>
         </div>
