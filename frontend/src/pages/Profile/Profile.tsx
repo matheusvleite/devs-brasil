45a6ab56efff8860a7ba0ed8c55dc1cfdab08656
unknown <matheusvictor2000@gmail.com>
2023-03-21 15:25:43 -0300

    Dá estrela na pagina individual do perfil do usuario

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,25 +1,39 @@
 import { useEffect } from "react";
+import { BsFillStarFill } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Loading from "../../components/Loading/Loading";
-import { userDetails } from "../../slices/userSlice";
+import { useAuth } from "../../hooks/useAuth";
+import { starAnUser, userDetails } from "../../slices/userSlice";
 import { AppDispatch, RootState } from "../../store";
 import { upload } from "../../utils/config";
 import styles from './Profile.module.css';
-import { BsFillStarFill } from "react-icons/bs";
 
 const Profile = () => {
 
   const { id } = useParams();
   const dispatch = useDispatch<AppDispatch>();
+  const { auth } = useAuth();
+
+  const navigate = useNavigate();
 
   const { user, loading } = useSelector((state: RootState) => state.user);
+  const { user: userAuth } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
 
     if (id) dispatch(userDetails(id))
 
   }, [dispatch, id])
+
+  const handleStar = (id: string) => {
+    if (!auth) {
+      navigate('/login')
+      return
+    }
+
+    dispatch(starAnUser(id))
+  }
 
 
   if (loading) {
@@ -35,7 +49,10 @@ const Profile = () => {
               <h1>{user.name}</h1>
               <span>{user.email}</span>
               <p>{user.area}</p>
-              {user.stars && user.stars.length}<BsFillStarFill />
+              <div className={styles.star}>
+                <span>{user.stars && user.stars.length}</span><BsFillStarFill />
+                {userAuth && user.stars.includes(String(userAuth._id)) ? '' : <button onClick={() => handleStar(user._id)}>Dá estrela?</button>}
+              </div>
             </div>
           </div>
           <div className={styles.bio}>
